a7427871c8580252cd94e712f831fabefe986b8b
Kayden Clark <kaydenclark725@gmail.com>
2019-12-10 11:14:58 -0700

    generate task filled todos

--- app.js
+++ app.js
@@ -109,6 +109,38 @@ const newList = (list) => {
     return iou
 }
 
+const readTaskById = (id) => {
+    let iou = new Promise((resolve, reject) => {
+        // Use connect method to connect to the server
+        MongoClient.connect(url, settings, function (err, client) {
+            if (err) {
+                reject(err)
+            } else {
+                console.log("Connected to server Read Contacts");
+
+                const db = client.db(dbName);
+                // Get the tasks collection
+                const collection = db.collection('ToDoTasks');
+                // Find some documents
+                collection.find({ _id: ObjectId(id) }).toArray(function (err, docs) {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        const results = {
+                            data: docs[0],
+                            msg: "Found the following records"
+                        }
+
+                        client.close();
+                        resolve(results);
+                    }
+                });
+            }
+        });
+    })
+    return iou;
+}
+
 const readTasks = () => {
     let iou = new Promise((resolve, reject) => {
     // Use connect method to connect to the server
@@ -154,12 +186,27 @@ const readLists = () => {
                 // Get the contacts collection
                 const collection = db.collection('ToDoLists');
                 // Find some documents
-                collection.find({}).toArray(function (err, docs) {
+                collection.find({}).toArray(async function (err, docs) {
                     if(err){
                         reject(err)
                     }else{
+                        const filterList = async () => {
+                            let listList = []
+                            for(i = 0; i < docs.length; i++){
+                                let todoTasks = []
+                                for(j = 0; j < docs[i].todos.length; j++){
+                                    const task = await readTaskById(docs[i].todos[j])
+                                    todoTasks.push(task.data.title)
+                                }
+                                listList.push(
+                                    {title : docs[i].title,
+                                    data : todoTasks})
+                            }
+                            return listList
+                        }
+                        
                         const results = {
-                            data: docs,
+                            data: await filterList(),
                             msg: "Found the following records"
                         }
                         
