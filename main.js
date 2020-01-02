9a4c6b1e74b4688289eaf0d051eb75de94b62acb
Kayden Clark <kaydenclark725@gmail.com>
2020-01-01 21:43:03 -0700

    pushing to heroku

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ const port = process.env.PORT || 5000
 
 app.use(express.json());
 app.use(cors());
+app.use(require('./routes'))
 
 app.get('/lists', async (req, res) => {
     lists = await readLists()
